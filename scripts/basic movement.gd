4c6f44ee46f523f7d4a14899bdb0e4b6da11c687
Zaine2k <165634822+Zaine2k@users.noreply.github.com>
2025-02-26 22:11:36 -0700

    new changes
    added double jump ya

--- scripts/basic movement.gd
+++ scripts/basic movement.gd
@@ -1,22 +1,26 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 250.0
+const JUMP_VELOCITY = -300.0
+var jump_count = 0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	else:
+		jump_count = 0
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and jump_count < 2:
+		jump_count += 1
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("key_left", "key_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
