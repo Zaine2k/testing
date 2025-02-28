888de6357bbd4c919d845e1ad75f305b2a9ed094
Zaine2k <165634822+Zaine2k@users.noreply.github.com>
2025-02-28 01:18:08 -0700

    initial bullet time
    add bullet time for 2 seconds on RMB

---
+++ scripts/bullet time controller.gd
@@ -0,0 +1,18 @@
+extends Node
+
+@export var normal_time_scale: float = 1.0
+@export var slowmo_time_scale: float = 0.5
+@export var slowmo_duration: float = 2.0  # Slow-motion lasts for 2 seconds
+
+var slowmo_active: bool = false
+
+func _input(event):
+	if event.is_action_pressed("bullet_time") and not slowmo_active:
+		start_slow_motion()
+
+func start_slow_motion():
+	slowmo_active = true
+	Engine.time_scale = slowmo_time_scale
+	await get_tree().create_timer(slowmo_duration * slowmo_time_scale).timeout
+	Engine.time_scale = normal_time_scale
+	slowmo_active = false
